2e268d134f9873a1307d456278701294c7240056
James Waples <jamwaffles@gmail.com>
2018-08-07 18:28:47 +0100

    Pass R2D2 pool when instanciating Postgres store

--- src/eventstore/postgres.rs
+++ src/eventstore/postgres.rs
@@ -1,8 +1,19 @@
 use event::Event;
 use eventstore::EventStore;
+use r2d2::Pool;
+use r2d2_postgres::PostgresConnectionManager;
 
 /// Postgres-backed implementation of an event store
-pub struct PostgresEventStore;
+pub struct PostgresEventStore {
+    pool: Pool<PostgresConnectionManager>,
+}
+
+impl PostgresEventStore {
+    /// Create a new Postgres-backed store
+    pub fn new(pool: Pool<PostgresConnectionManager>) -> Self {
+        Self { pool }
+    }
+}
 
 impl<E> EventStore<E> for PostgresEventStore
 where
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,15 @@
 
 #![deny(missing_docs)]
 
+#[feature(postgres_support)]
+extern crate r2d2;
+#[feature(postgres_support)]
+extern crate r2d2_postgres;
+
 mod event;
 mod eventstore;
 
 pub use event::Event;
-pub use eventstore::{EventStore, PostgresEventStore};
+pub use eventstore::EventStore;
+#[feature(postgres_support)]
+pub use eventstore::PostgresEventStore;
