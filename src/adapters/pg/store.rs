3608baf0c746bfd570adf9097af8f64b2907acc8
Peter East <petereast@users.noreply.github.com>
2018-08-27 17:25:45 +0100

    Refactor query generator and add tests (#22)

--- src/adapters/pg/store.rs
+++ src/adapters/pg/store.rs
@@ -20,13 +20,36 @@ pub struct PgStoreAdapter<'a, E> {
     conn: &'a Connection,
 }
 
-impl<'a, E> PgStoreAdapter<'a, E> {
+impl<'a, E> PgStoreAdapter<'a, E>
+where
+    E: Events,
+{
     /// Create a new PgStore from a Postgres DB connection
     pub fn new(conn: &'a Connection) -> Self {
         Self {
             conn,
             phantom: PhantomData,
         }
+    }
+
+    fn generate_query<T, A>(
+        query_string: &PgQuery<'a>,
+        since: Option<CacheResult<T>>,
+    ) -> (T, String)
+    where
+        T: Aggregator<E, A, PgQuery<'a>> + Default,
+        A: Clone,
+    {
+        let (initial_state, query_string) = if let Some((existing, time)) = since {
+            (existing, format!(
+                "SELECT * FROM ({}) AS events WHERE events.context->>'time' >= '{}' ORDER BY events.context->>'time' ASC",
+                query_string.query, time
+            ))
+        } else {
+            (T::default(), String::from(query_string.query))
+        };
+
+        (initial_state, query_string)
     }
 }
 
@@ -40,15 +63,7 @@ where
         A: Clone,
     {
         let q = T::query(query_args);
-
-        let (initial_state, query_string) = if let Some((existing, time)) = since {
-            (existing, format!(
-                "SELECT * FROM ({}) AS events WHERE events.context->>'time' >= '{}' ORDER BY events.context->>'time' ASC",
-                q.query, time
-            ))
-        } else {
-            (T::default(), String::from(q.query))
-        };
+        let (initial_state, query_string) = Self::generate_query(&q, since);
 
         let trans = self.conn.transaction().expect("Tranny");
         let stmt = trans.prepare(&query_string).expect("Prep");
@@ -99,5 +114,45 @@ where
             ).expect("Save");
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use testhelpers::*;
+
+    #[test]
+    fn it_generates_a_query_when_there_is_no_cache() {
+        let q = TestCounterEntity::query("something".into());
+        let since = None;
+
+        let (state, query_string): (TestCounterEntity, String) =
+            PgStoreAdapter::generate_query(&q, since);
+
+        let expected_query = "select * from events where data->>'ident' = $1";
+
+        assert_eq!(state, TestCounterEntity::default());
+
+        assert_eq!(query_string, expected_query);
+    }
+
+    #[test]
+    fn it_generates_a_different_query_when_there_is_a_cache() {
+        let q = TestCounterEntity::query("something".into());
+        let since: Option<CacheResult<TestCounterEntity>> = Some((
+            TestCounterEntity::default(),
+            Utc.ymd(2018, 8, 27).and_hms(12, 43, 52),
+        ));
+
+        let (state, query_string) = PgStoreAdapter::generate_query(&q, since);
+
+        let base_query = "select * from events where data->>'ident' = $1";
+        let generated_query = format!(
+            "SELECT * FROM ({}) AS events WHERE events.context->>'time' >= '{}' ORDER BY events.context->>'time' ASC",
+            base_query, "2018-08-27 12:43:52 UTC");
+        assert_eq!(state, TestCounterEntity::default()); // What does this end up being?
+
+        assert_eq!(query_string, generated_query);
     }
 }
