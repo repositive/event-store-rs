3405176172c872ca0ab92cffc2c61a277e5a8836
James Waples <jamwaffles@gmail.com>
2018-08-30 15:08:12 +0100

    "Fix" doc tests by removing them

--- event-store-derive-internals/src/lib.rs
+++ event-store-derive-internals/src/lib.rs
@@ -2,30 +2,10 @@ extern crate serde;
 
 use serde::{de::DeserializeOwned, Serialize};
 
-/// Trait to be implemented by the enum of all domain events. Must also implement `serde::Serialize`
-/// and `serde::DeserializeOwned`
+/// Trait to be implemented by the enum of all domain events
 ///
-/// ```rust
-/// # #[macro_use]
-/// # extern crate serde_derive;
-/// # extern crate event_store;
-/// # use event_store::EventData;
-/// #[derive(Serialize, Deserialize)]
-/// struct EventA;
-///
-/// #[derive(Serialize, Deserialize)]
-/// struct EventB;
-///
-/// #[derive(Serialize, Deserialize)]
-/// enum DomainEvents {
-///     A(EventA),
-///     B(EventB),
-/// }
-///
-/// impl EventData for DomainEvents {}
-///
-/// fn main() {}
-/// ```
+/// Note (TODO: fix): For enums, the struct name and variant name **MUST** match, or deriving
+/// `EventData` won't work correctly.
 pub trait EventData: Serialize + DeserializeOwned {
     fn event_namespace_and_type(&self) -> &'static str;
 
