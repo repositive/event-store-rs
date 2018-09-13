53b93274a2f11d02f3c88fb6a5930f7894c559ce
Ruben Paz <me@ruben.io>
2018-09-13 15:18:10 +0100

    EventData is Clone and Debug

--- event-store-derive-internals/src/lib.rs
+++ event-store-derive-internals/src/lib.rs
@@ -1,9 +1,10 @@
 extern crate serde;
 
 use serde::{de::DeserializeOwned, Serialize};
+use std::fmt::Debug;
 
 /// Trait to be implemented by all domain events
-pub trait EventData: Serialize + DeserializeOwned {
+pub trait EventData: Clone + Sync + Debug + Serialize + DeserializeOwned {
     fn event_namespace_and_type() -> &'static str;
 
     fn event_namespace() -> &'static str;
