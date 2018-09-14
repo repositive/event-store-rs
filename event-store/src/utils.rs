98cad5f6a2359186aab5ec6d1a82393cd4ca0306
Peter East <peter@repositive.io>
2018-09-14 10:31:17 +0100

    Add Arced futures and streams

--- event-store/src/utils.rs
+++ event-store/src/utils.rs
@@ -1,6 +1,9 @@
 use futures::future::Future;
 use futures::stream::Stream;
+use std::sync::Arc;
 
-pub type BoxedFuture<'a, T, E> = Box<Future<Item = T, Error = E> + Send + 'a>;
+pub type BoxedFuture<'a, T, E> = Box<Future<Item = T, Error = E> + Send + Sync + 'a>;
+pub type ArcFuture<'a, T, E> = Arc<Future<Item = T, Error = E> + 'a>;
 
-pub type BoxedStream<'a, T, E> = Box<Stream<Item = T, Error = E> + Send + 'a>;
+pub type BoxedStream<'a, T, E> = Box<Stream<Item = T, Error = E> + Send + Sync + 'a>;
+pub type ArcStream<'a, T, E> = Arc<Stream<Item = T, Error = E> + 'a>;
