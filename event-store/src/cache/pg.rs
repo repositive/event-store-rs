f8654c17576292b0e17f804f9e9c52fe6adf9b40
James Waples <jamwaffles@gmail.com>
2018-11-11 17:14:25 +0000

    Remove cache prepared statements for now

--- event-store/src/cache/pg.rs
+++ event-store/src/cache/pg.rs
@@ -3,41 +3,58 @@
 use super::CacheResult;
 use chrono::prelude::*;
 use postgres::stmt::Statement;
+use r2d2::Pool;
 use r2d2::PooledConnection;
 use r2d2_postgres::PostgresConnectionManager;
 use serde::{de::DeserializeOwned, Serialize};
 use serde_json::{from_value, to_value};
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
-pub struct PgCacheAdapter<'a> {
-    set_stmt: &'a Statement<'a>,
-    get_stmt: &'a Statement<'a>,
+pub struct PgCacheAdapter {
+    // set_stmt: &'a Statement<'a>,
+    // get_stmt: &'a Statement<'a>,
+    conn: Pool<PostgresConnectionManager>,
 }
 
-impl<'a> PgCacheAdapter<'a> {
-    pub fn new(set_stmt: &'a Statement<'a>, get_stmt: &'a Statement<'a>) -> Self {
-        Self { set_stmt, get_stmt }
+impl PgCacheAdapter {
+    // pub fn new(set_stmt: &'a Statement<'a>, get_stmt: &'a Statement<'a>) -> Self {
+    //     Self { set_stmt, get_stmt }
+    // }
+
+    pub fn new(conn: Pool<PostgresConnectionManager>) -> Self {
+        Self { conn }
     }
 
-    pub fn prepare_statements(
-        conn: &'a PooledConnection<PostgresConnectionManager>,
-    ) -> (Statement<'a>, Statement<'a>) {
-        (
-            conn.prepare_cached(
+    // pub fn prepare_statements(
+    //     conn: &'a PooledConnection<PostgresConnectionManager>,
+    // ) -> (Statement<'a>, Statement<'a>) {
+    //     (
+    //         conn.prepare_cached(
+    //             r#"INSERT INTO aggregate_cache (id, data, time)
+    //         VALUES ($1, $2, NOW())
+    //         ON CONFLICT (id)
+    //         DO UPDATE SET data = EXCLUDED.data, time = now() RETURNING data"#,
+    //         )
+    //         .unwrap(),
+    //         conn.prepare_cached(r#"SELECT data, time FROM aggregate_cache WHERE id = $1 LIMIT 1"#)
+    //             .unwrap(),
+    //     )
+    // }
+
+    pub fn set<V: Serialize + Debug>(&self, key: String, value: V) -> Result<(), String> {
+        trace!("Set cache key {}: {:?}", key, value);
+
+        self.conn
+            .get()
+            .unwrap()
+            .execute(
                 r#"INSERT INTO aggregate_cache (id, data, time)
-            VALUES ($1, $2, NOW())
-            ON CONFLICT (id)
-            DO UPDATE SET data = EXCLUDED.data, time = now() RETURNING data"#,
+                    VALUES ($1, $2, NOW())
+                    ON CONFLICT (id)
+                    DO UPDATE SET data = EXCLUDED.data, time = now() RETURNING data"#,
+                &[&key, &to_value(value).expect("To value")],
             )
-            .unwrap(),
-            conn.prepare_cached(r#"SELECT data, time FROM aggregate_cache WHERE id = $1 LIMIT 1"#)
-                .unwrap(),
-        )
-    }
-
-    pub fn set<V: Serialize>(&self, key: String, value: V) -> Result<(), String> {
-        self.set_stmt
-            .execute(&[&key, &to_value(value).expect("To value")])
             .map(|_| ())
             .map_err(|_| "Failed to set cache item".into())
     }
@@ -46,9 +63,16 @@ impl<'a> PgCacheAdapter<'a> {
     where
         T: DeserializeOwned,
     {
+        trace!("Get cache key {}", key);
+
         let rows = self
-            .get_stmt
-            .query(&[&key])
+            .conn
+            .get()
+            .unwrap()
+            .query(
+                r#"SELECT data, time FROM aggregate_cache WHERE id = $1 LIMIT 1"#,
+                &[&key],
+            )
             .map_err(|e| format!("Retrieve cache: {:?}", e))?;
 
         // `rows.get()` panics if index is out of bounds, hence this check
