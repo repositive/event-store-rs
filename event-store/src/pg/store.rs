be1180f1442c0bda5ae06d71e15d9446eb9ba090
James Waples <james@wapl.es>
2018-12-05 14:02:45 +0000

    Add PG cache

--- event-store/src/pg/store.rs
+++ event-store/src/pg/store.rs
@@ -1,69 +1,18 @@
 use crate::event::Event;
-use crate::store_query::StoreQuery;
+use crate::pg::PgQuery;
 use chrono::prelude::*;
 use event_store_derive_internals::EventData;
 use event_store_derive_internals::Events;
 use fallible_iterator::FallibleIterator;
 use futures::future;
-use futures::future::IntoFuture;
-use futures::stream::Stream;
 use futures::Future;
-use postgres::Connection;
-use r2d2::{self, Pool, PooledConnection};
+use r2d2::{self, PooledConnection};
 use r2d2_postgres::postgres::types::ToSql;
-use r2d2_postgres::{PostgresConnectionManager, TlsMode};
+use r2d2_postgres::PostgresConnectionManager;
 use serde_json::to_value;
 use serde_json::{from_value, Value as JsonValue};
-use sha2::{Digest, Sha256};
-use std::io::{self, ErrorKind};
-use std::net::SocketAddr;
-use std::str;
-use tokio::net::TcpStream;
-use tokio_core::reactor::Core;
+use std::io;
 use uuid::Uuid;
-
-/// Representation of a Postgres query and args
-#[derive(Debug)]
-pub struct PgQuery {
-    /// Query string with placeholders
-    pub query: String,
-
-    /// Arguments to use for the query
-    pub args: Vec<Box<ToSql>>,
-}
-
-impl PgQuery {
-    /// Create a new query from a query string and arguments
-    pub fn new(query: &str, args: Vec<Box<ToSql>>) -> Self {
-        Self {
-            query: query.into(),
-            args,
-        }
-    }
-}
-
-impl StoreQuery for PgQuery {
-    fn unique_id(&self) -> String {
-        let hash = Sha256::digest(format!("{:?}:[{}]", self.args, self.query).as_bytes());
-        hash.iter().fold(String::new(), |mut acc, hex| {
-            acc.push_str(&format!("{:X}", hex));
-            acc
-        })
-    }
-}
-
-/// Connect to a local Postgres database on port 5430
-pub fn pg_connect() -> Pool<PostgresConnectionManager> {
-    let manager = PostgresConnectionManager::new(
-        "postgres://postgres@localhost:5430/eventstorerust",
-        TlsMode::None,
-    )
-    .unwrap();
-
-    let pool = r2d2::Pool::new(manager).unwrap();
-
-    pool
-}
 
 /// Save an event into PG
 pub fn pg_save<ED>(
---
+++ event-store/tests/cache_set_get.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate log;
+extern crate event_store;
+extern crate pretty_env_logger;
+
+use event_store::*;
+use futures::future::Future;
+use tokio::runtime::Runtime;
+
+#[test]
+fn cache_set_get() {
+    pretty_env_logger::init();
+
+    let test_entity = TestCounterEntity { counter: 100 };
+
+    trace!("Save and emit test");
+
+    let conn = pg_create_random_db();
+
+    let rt = Runtime::new().unwrap();
+
+    let run = pg_cache_save(conn.get().unwrap(), "_test".into(), &test_entity)
+        .and_then(move |_| pg_cache_read::<TestCounterEntity>(conn.get().unwrap(), "_test".into()));
+
+    let res = rt.block_on_all(run).unwrap();
+
+    assert_eq!(res.unwrap().0, test_entity);
+}
