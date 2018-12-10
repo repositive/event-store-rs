862874aa7c3f4a3cdc74e41c2887447382bc773d
James Waples <james@wapl.es>
2018-12-10 16:02:30 +0000

    Add a bunch of logging

--- event-store/src/store.rs
+++ event-store/src/store.rs
@@ -11,7 +11,7 @@ use event_store_derive_internals::EventData;
 use event_store_derive_internals::Events;
 use futures::Future;
 use lapin_futures::channel::Channel;
-use log::{error, trace};
+use log::{debug, error, trace};
 use r2d2::Pool;
 use r2d2::PooledConnection;
 use r2d2_postgres::PostgresConnectionManager;
@@ -48,24 +48,37 @@ impl Store {
                 store_namespace,
                 pool,
             })
-            .and_then(|store| store.subscribe::<EventReplayRequested>().map(|_| store))
+            .and_then(|store| {
+                debug!("Begin listening for event replay requests");
+
+                store.subscribe::<EventReplayRequested>().map(|_| store)
+            })
     }
 
     pub fn aggregate<T, QA, E>(&self, query_args: QA) -> impl Future<Item = T, Error = io::Error>
     where
         E: Events,
         T: Aggregator<E, QA, PgQuery>,
-        QA: Clone,
+        QA: Clone + Debug,
     {
+        debug!("Aggregate with arguments {:?}", query_args);
+
         let store_query = T::query(query_args.clone());
         let cache_key = store_query.unique_id();
+        let debug_cache_key = cache_key.clone();
 
         pg_cache_read(self.pool.get().unwrap(), cache_key)
             .and_then(
-                |(conn, cache_result): (
+                move |(conn, cache_result): (
                     PooledConnection<PostgresConnectionManager>,
                     Option<CacheResult<T>>,
                 )| {
+                    trace!(
+                        "Aggregate cache key {} result {:?}",
+                        debug_cache_key,
+                        cache_result
+                    );
+
                     let (initial_state, since) = cache_result
                         .map(|res| (res.0, Some(res.1)))
                         .unwrap_or_else(|| (T::default(), None));
@@ -88,8 +101,10 @@ impl Store {
 
     pub fn save<ED>(&self, event: Event<ED>) -> impl Future<Item = (), Error = io::Error>
     where
-        ED: EventData,
+        ED: EventData + Debug,
     {
+        debug!("Save event {:?}", event);
+
         let queue_name = self.event_queue_name::<ED>();
 
         let _channel = self.channel.clone();
@@ -101,7 +116,7 @@ impl Store {
 
     pub fn subscribe<ED>(&self) -> impl Future<Item = (), Error = io::Error>
     where
-        ED: EventHandler + 'static,
+        ED: EventHandler + Debug + 'static,
     {
         let queue_name = self.event_queue_name::<ED>();
         let queue_name_2 = queue_name.clone();
@@ -109,6 +124,8 @@ impl Store {
         let inner_self = self.clone();
         let inner_channel = self.channel.clone();
         let inner_channel_2 = inner_channel.clone();
+
+        debug!("Begin listening for events on queue {}", queue_name);
 
         let consumer = amqp_create_consumer(
             inner_channel,
@@ -129,11 +146,15 @@ impl Store {
 
         pg_last_event::<ED>(self.pool.get().unwrap())
             .map(|last_event| {
+                trace!("Fetched last event {:?}", last_event);
+
                 last_event.map(|ev| ev.context.time).unwrap_or_else(|| {
                     DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(0, 0), Utc)
                 })
             })
             .and_then(move |since| {
+                trace!("Emit replay request for events since {:?}", since);
+
                 amqp_emit_event(
                     inner_channel_2,
                     queue_name_2,
