c4ef53ebbc97417c214705f56c167169d4ef6b81
James Waples <james@wapl.es>
2018-12-10 16:30:25 +0000

    Add some debug logging to test

--- event-store/tests/emit_and_receive.rs
+++ event-store/tests/emit_and_receive.rs
@@ -1,7 +1,7 @@
 use event_store::Event;
 use event_store::*;
 use futures::future::Future;
-use log::trace;
+use log::{debug, info};
 use std::io;
 use std::time::{Duration, Instant};
 use tokio::runtime::Runtime;
@@ -13,19 +13,28 @@ fn save_and_emit() {
 
     let test_event = TestEvent { num: 100 };
 
-    trace!("Save and emit test");
+    info!("Save and emit test");
 
     let pool = pg_create_random_db();
 
     let mut rt = Runtime::new().unwrap();
 
     let run = Store::new("store_namespace".into(), pool)
-        .and_then(|store| store.subscribe::<TestEvent>().map(|_| store))
-        .and_then(|store| store.save(Event::from_data(test_event)).map(|_| store))
+        .and_then(|store| {
+            debug!("Store created");
+
+            store.subscribe::<TestEvent>().map(|_| store)
+        })
+        .and_then(|store| {
+            debug!("Subscription created");
+            store.save(Event::from_data(test_event)).map(|_| store)
+        })
         .and_then(|_| {
+            debug!("Store saved");
             Delay::new(Instant::now() + Duration::from_millis(100))
                 .map_err(|_| io::Error::new(io::ErrorKind::Other, "wait error"))
-        });
+        })
+        .map(|_| debug!("Chain complete"));
 
     rt.block_on(run).unwrap();
 }
