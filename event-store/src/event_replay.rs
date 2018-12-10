38d255fb91e533bb74869994c46aec7b1a737c52
James Waples <james@wapl.es>
2018-12-10 15:01:43 +0000

    Listen/emit EventReplayRequested events

--- event-store/src/event_replay.rs
+++ event-store/src/event_replay.rs
@@ -3,6 +3,7 @@ use crate::event_handler::EventHandler;
 use crate::store::Store;
 use chrono::prelude::*;
 use event_store_derive::*;
+use event_store_derive_internals::EventData;
 use log::debug;
 use serde_derive::*;
 
@@ -12,6 +13,19 @@ pub struct EventReplayRequested {
     requested_event_namespace: String,
     requested_event_type: String,
     since: DateTime<Utc>,
+}
+
+impl EventReplayRequested {
+    pub fn from_event<ED>(since: DateTime<Utc>) -> Event<Self>
+    where
+        ED: EventData,
+    {
+        Event::from_data(Self {
+            requested_event_namespace: ED::event_namespace().to_string(),
+            requested_event_type: ED::event_type().to_string(),
+            since,
+        })
+    }
 }
 
 impl EventHandler for EventReplayRequested {
