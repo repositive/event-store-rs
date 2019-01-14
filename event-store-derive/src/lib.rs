9800d33ad3a0dec74d0074282c83353469437258
James Waples <james@wapl.es>
2019-01-14 22:34:19 +0000

    Support serde attributes on EventData structs

--- event-store-derive/src/lib.rs
+++ event-store-derive/src/lib.rs
@@ -15,14 +15,14 @@ mod ns;
 
 const PROC_MACRO_NAME: &'static str = "event_store";
 
-#[proc_macro_derive(Events, attributes(event_store))]
+#[proc_macro_derive(Events, attributes(event_store, serde))]
 pub fn derive_events(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).unwrap();
 
     ns::expand_derive_namespace(&input).into()
 }
 
-#[proc_macro_derive(EventData, attributes(event_store))]
+#[proc_macro_derive(EventData, attributes(event_store, serde))]
 pub fn derive_eventdata(input: TokenStream) -> TokenStream {
     let input: DeriveInput = syn::parse(input).unwrap();
 
