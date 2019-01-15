1eb6ef0e6ce4b22eaf2388ee076af3b9b96189b3
James Waples <james@wapl.es>
2019-01-15 20:09:14 +0000

    Remove old crappy event store

--- _event-store/src/store_query.rs
+++
@@ -1,9 +0,0 @@
-/// A query to be passed to the store
-///
-/// This trait must be implemented for whichever type you want to pass to a particular store. See
-/// impls below for examples.
-pub trait StoreQuery {
-    /// You must return a unique identifier based on the query you are performing. This identifier
-    /// will then be used to identify the cache and optimize the aggregations using memoization
-    fn unique_id(&self) -> String;
-}
--- _event-store/src/utils.rs
+++
@@ -1,3 +0,0 @@
-use futures::future::Future;
-
-pub type BoxedFuture<'a, T, E> = Box<Future<Item = T, Error = E> + Send + 'a>;
