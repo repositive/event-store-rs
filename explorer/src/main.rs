b270d44295b1fa6130344a1071b06ce5f6b2bb51
James Waples <james@wapl.es>
2019-01-17 20:38:59 +0000

    Parse DB, namespace and event type from CLI args

--- explorer/src/main.rs
+++ explorer/src/main.rs
@@ -9,15 +9,27 @@ use event_store::{
     EventContext, SubscribableStore,
 };
 use gtk::prelude::*;
-use log::info;
+use log::{debug, info, trace};
 use r2d2_postgres::{PostgresConnectionManager, TlsMode};
 use serde_derive::Deserialize;
 use serde_json::Value as JsonValue;
 use std::io;
 use std::net::SocketAddr;
+use structopt::StructOpt;
 use uuid::Uuid;
 
-#[derive(Deserialize)]
+#[derive(StructOpt, Debug)]
+#[structopt(name = "explorer")]
+struct CliOpts {
+    #[structopt(name = "DATABASE")]
+    database: String,
+    #[structopt(name = "NAMESPACE")]
+    event_namespace: String,
+    #[structopt(name = "EVENT")]
+    event_type: String,
+}
+
+#[derive(Deserialize, Debug)]
 struct AnyEvent {
     id: Uuid,
     data: JsonValue,
@@ -36,16 +48,17 @@ fn add_column(list: &gtk::TreeView, ty: &str, title: &str, idx: i32) {
     list.append_column(&column);
 }
 
-async fn create_store() -> Result<SubscribableStore, io::Error> {
+async fn create_store(db: &String) -> Result<SubscribableStore, io::Error> {
     let manager = PostgresConnectionManager::new(
-        "postgres://repositive:repositive@localhost:5432/organisations",
+        format!("postgres://repositive:repositive@localhost:5432/{}", db),
         TlsMode::None,
-    )
-    .unwrap();
+    )?;
 
-    let pool = r2d2::Pool::new(manager).unwrap();
+    let pool = r2d2::Pool::new(manager).expect("Could not create pool");
 
-    let addr: SocketAddr = "127.0.0.1:5672".parse().unwrap();
+    let addr: SocketAddr = "127.0.0.1:5672"
+        .parse()
+        .expect("Could not parse RabbitMQ address");
 
     let store_adapter = await!(PgStoreAdapter::new(pool.clone()))?;
     let cache_adapter = await!(PgCacheAdapter::new(pool.clone()))?;
@@ -65,31 +78,27 @@ async fn create_store() -> Result<SubscribableStore, io::Error> {
 fn main() {
     pretty_env_logger::init();
 
-    // MASSIVE issue with some timestamps currently in the DB: they're not valid! ISO8601 is
-    // somewhat lax, but Chrono tries to parse as either RFC2822 or RFC3339. We could attempt a
-    // custom parse with [parse_from_str](https://docs.rs/chrono/0.4.6/chrono/struct.DateTime.html#method.parse_from_str)
-    // but why not just fix the DB so it's correct? I think most cases are just missing the last two
-    // digits of a 4 digit timezone.
-    // let test: DateTime<Utc> = "2019-01-16T22:07:04.845Z".parse().expect("Could not parse test 1");
-    // let test2: DateTime<FixedOffset> = DateTime::parse_from_rfc2822("2018-10-10T19:52:21+00").expect("Could not parse test 2");
-    // let test2: DateTime<FixedOffset> = DateTime::parse_from_rfc3339("2018-10-10T19:52:21+00").expect("Could not parse test 3");
-    // let test3: DateTime<Utc> = "2018-10-10T19:52:21+00".parse().unwrap();
-
     tokio::run_async(
         async {
-            let store = await!(create_store()).unwrap();
+            let opts = CliOpts::from_args();
+
+            debug!("{:?}", opts);
+
+            let store = await!(create_store(&opts.database)).expect("Could not get store");
 
             let forever = Utc.ymd(1970, 1, 1).and_hms(0, 0, 0);
 
             let test_events = await!(store.internals_get_store().read_events_since(
-                "organisations",
-                "PolicyUpdated",
+                &opts.event_namespace,
+                &opts.event_type,
                 forever
             ))
-            .unwrap()
+            .expect("Could not create store")
             .into_iter()
             .map(|res| {
-                let evt: AnyEvent = serde_json::from_value(res).unwrap();
+                trace!("Event ID {}", res["id"]);
+
+                let evt: AnyEvent = serde_json::from_value(res).expect("Failed to parse");
 
                 evt
             })
@@ -104,9 +113,10 @@ fn main() {
             let glade_src = include_str!("glade/main.glade");
             let builder = gtk::Builder::new_from_string(glade_src);
 
-            let window: gtk::Window = builder.get_object("window-main").unwrap();
+            let window: gtk::Window = builder.get_object("window-main").expect("window-main");
 
-            let results_list: gtk::TreeView = builder.get_object("list-results").unwrap();
+            let results_list: gtk::TreeView =
+                builder.get_object("list-results").expect("list-results");
             let results_store =
                 gtk::ListStore::new(&[gtk::Type::String, gtk::Type::String, gtk::Type::String]);
 
@@ -118,6 +128,8 @@ fn main() {
             results_list.set_model(Some(&results_store));
 
             for evt in test_events {
+                trace!("Event {:?}", evt);
+
                 results_store.insert_with_values(
                     None,
                     &[0, 1, 2],
