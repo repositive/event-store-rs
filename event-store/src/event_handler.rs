e9db1b892c8e613711886632a3d91b22b9f2feb0
Leo Jpod <leojpod@users.noreply.github.com>
2019-02-05 15:03:02 +0100

    [F] Provide default implementation for EventHandler::handle_event (#77)
    * #76 - Provide default implementation for EventHandler::handle_event
    
    * add some sort of test to it

--- event-store/src/event_handler.rs
+++ event-store/src/event_handler.rs
@@ -7,5 +7,5 @@ use event_store_derive_internals::EventData;
 /// Event handler trait
 pub trait EventHandler: Sized + EventData {
     /// The method called when an incoming event is received
-    fn handle_event(event: Event<Self>, saver: &Store);
+    fn handle_event(_event: Event<Self>, _saver: &Store) {}
 }
