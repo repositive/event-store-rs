a893b88794785980f8283d7658ffc963408e0aa6
James Waples <james@wapl.es>
2019-05-07 15:50:39 +0100

    Map empty subject to '{}' in DB
    For backwards compat reasons, '{}' should deserialize to None. Likewise,
    None should serialize to '{}'. This should really be stored as null
    instead of an empty object, but the decision was made a long time ago
    and there is a lot of Typescript out there that depends on subject being
    at least an object.

--- unifier/src/event.rs
+++ unifier/src/event.rs
@@ -1,10 +1,10 @@
 use chrono::prelude::*;
-use serde_derive::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::collections::HashMap;
 use uuid::Uuid;
 
 /// Event data
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug, Clone)]
 pub struct EventData {
     pub event_namespace: String,
     pub event_type: String,
@@ -18,12 +18,54 @@ pub struct EventData {
     legacy_type: String,
 }
 
+/// Serialize an optional subject so that a `None` value serializes to the empty object `{}`
+fn serialize_subject<S>(
+    subject: &Option<HashMap<String, serde_json::Value>>,
+    serializer: S,
+) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    if let Some(subject) = subject {
+        subject.serialize(serializer)
+    } else {
+        serde_json::json!({}).serialize(serializer)
+    }
+}
+
+/// Convert an empty object `{}` to `None` for subjects
+fn deserialize_subject<'de, D>(
+    deserializer: D,
+) -> Result<Option<HashMap<String, serde_json::Value>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let res = Option::deserialize(deserializer)?.and_then(
+        |hashmap: HashMap<String, serde_json::Value>| {
+            if hashmap.len() > 0 {
+                Some(hashmap)
+            } else {
+                None
+            }
+        },
+    );
+
+    Ok(res)
+}
+
 /// Event context
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug, Clone)]
 pub struct EventContext {
     pub action: Option<String>,
 
     /// Event "subject" or metadata
+    ///
+    /// For backwards compatibility reasons, if the value of `subject` is `None`, it serializes to
+    /// an empty object `{}`. If an empty object is encountered during deserialization it is mapped
+    /// to `None`.
+    #[serde(default)]
+    #[serde(serialize_with = "serialize_subject")]
+    #[serde(deserialize_with = "deserialize_subject")]
     pub subject: Option<HashMap<String, serde_json::Value>>,
 
     /// Event creation time
@@ -31,7 +73,7 @@ pub struct EventContext {
 }
 
 /// An event
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug, Clone)]
 pub struct Event {
     pub id: Uuid,
     pub data: EventData,
