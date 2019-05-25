91843f9e7ebc7eba961594f689b27a8ad6e32b6d
James Waples <james@wapl.es>
2019-05-25 12:05:27 +0100

    Remove unused Redis cache adapter

--- event-store/src/adapters/cache/mod.rs
+++ event-store/src/adapters/cache/mod.rs
@@ -1,11 +1,9 @@
 use chrono::prelude::*;
 
 mod pg;
-mod redis;
 
 // TODO: Rename this. `Result` implies an error condition, but it's not. Maybe `CacheItem`? Idk.
 /// Result of a cache search
 pub type CacheResult<T> = (T, DateTime<Utc>);
 
 pub use self::pg::PgCacheAdapter;
-pub use self::redis::RedisCacheAdapter;
