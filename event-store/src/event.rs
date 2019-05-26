d29ff11423de0b1e526966c9cf4e07c3272e5a67
James Waples <james@wapl.es>
2019-05-26 12:56:17 +0100

    Make sequence_number optional

--- event-store/src/event.rs
+++ event-store/src/event.rs
@@ -5,7 +5,7 @@ use uuid::Uuid;
 #[derive(serde_derive::Serialize, serde_derive::Deserialize, Debug, Clone)]
 pub struct Event<D> {
     pub id: Uuid,
-    pub sequence_number: u64,
+    pub sequence_number: Option<u64>,
     pub entity_id: Uuid,
     pub created_at: DateTime<Utc>,
     pub data: Option<D>,
